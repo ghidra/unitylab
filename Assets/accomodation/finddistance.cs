59ae94ce70ae156c65022e983c4b7d939b9438cb
ghidra <ghidra@gmail.com>
2019-12-14 16:47:21 -0500

    almost have a working system for accomodation

--- Assets/accomodation/finddistance.cs
+++ Assets/accomodation/finddistance.cs
@@ -2,12 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class finddistance : MonoBehaviour
 {
 	public Camera cam;
-    public Transform debug;    
+    public Transform debug;
+    public Transform debugSection;     
     private Vector3 focus_position;
     private Vector3 last_focus_position;
+
+    public enum _findtype
+    {
+        angle,
+        basic,
+        ray,
+        angleAndRay,
+        angleAndBasic
+    }
+
+    [System.Serializable]
+    public struct _sections
+    {
+        public GameObject section;
+        public _findtype findtype;
+        public GameObject[] children;   
+    }
+
+    
+    public _sections[] sections;
+    private int _inSection = -1;
 
 
     void Start()
@@ -20,12 +44,93 @@ public class finddistance : MonoBehaviour
         Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit)){
-            //print("I'm looking at " + hit.transform.name);
-            focus_position = Vector3.Lerp(last_focus_position,hit.point,0.6f);
-            last_focus_position = focus_position;
+            //lopp the sections, to see if we hit a new one
+            int i=0;
+            Vector3 hitPosition = new Vector3();
+            foreach(_sections section in sections)
+            {
+                if (hit.transform.gameObject == section.section)
+                {
+                    _inSection = i;
+                    hitPosition = hit.point;
+                    if(debugSection!=null)
+                    {
+                        debugSection.position = hitPosition;
+                    }
+                }
+                i++;
+            }
+            
+            ///move to section
+            /*if(hitPosition.magnitude>0f)
+            {
+                //print("I'm looking at " + hit.transform.name);
+                SetFocusPosition(hit.point);
+            }*/
 
-            debug.position = focus_position;
+        }
+        ///we are in a section
+        if(_inSection>=0)
+        {
+            //lets look at the sections children to focus on
+            switch(sections[_inSection].findtype)
+            {
+                case _findtype.angle:
+                    //print("angle type");
+                    SetFocusPosition( CheckAngle(ray,sections[_inSection].children));
+                    break;
+                case _findtype.basic:
+                    //print("basic type");
+                    SetFocusPosition(sections[_inSection].children[0].transform.position);
+                    break;
+                case _findtype.ray:
+                    print("ray type");
+                    SetFocusPosition(CheckRaycast(hit.point+ray.direction*0.01f,ray.direction));
+                    break;
+                case _findtype.angleAndRay:
+                    //print("angle and ray type");
+                    break;
+                case _findtype.angleAndBasic:
+                    //print("angle and basic type");
+                    break;
+            }
         }
         
+    }
+    private void SetFocusPosition(Vector3 pos)
+    {
+        focus_position = Vector3.Lerp(last_focus_position,pos,0.6f);
+        last_focus_position = focus_position;
+
+        if(debug!=null)
+        {
+            debug.position = focus_position;
+        }
+    }
+    private Vector3 CheckRaycast(Vector3 origin, Vector3 direction)
+    {
+        Ray ray = new Ray(origin,direction);//cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        RaycastHit hit;
+        Vector3 hitPosition = new Vector3();
+        if (Physics.Raycast(ray, out hit)){
+            hitPosition = hit.point;
+        }
+        return hitPosition;
+    }
+    private Vector3 CheckAngle(Ray ray, GameObject[] objects)
+    {
+        float dot = 0f;
+        Vector3 inView = new Vector3();
+        foreach(GameObject o in objects)
+        {
+            Vector3 oDir = o.transform.position-ray.origin;
+            float oDot = Vector3.Dot(ray.direction,oDir.normalized);
+            if(oDot>dot)
+            {
+                dot=oDot;
+                inView = o.transform.position;
+            }
+        }
+        return inView;
     }
 }
---
+++ Assets/accomodation/tmp_swimming.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tmp_swimming : MonoBehaviour
+{
+	private Vector3 _StartPosition;
+	[Range(0,1)]
+	public float speed=1.0f;
+	[Range(0,10)]
+	public float distance=1.0f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        _StartPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	float nx = Mathf.PerlinNoise(_StartPosition.x+Time.time*speed,_StartPosition.y+Time.time*speed);
+        float ny = Mathf.PerlinNoise(_StartPosition.x+Time.time*speed-1232.331f,_StartPosition.y+Time.time*speed+45.909f);
+        float nz = Mathf.PerlinNoise(_StartPosition.x+Time.time*speed+137.137f,_StartPosition.y+Time.time*speed+8845.099f);
+
+        Vector3 np = new Vector3(nx,ny,nz);
+        transform.position = np * distance + _StartPosition;
+    }
+}
